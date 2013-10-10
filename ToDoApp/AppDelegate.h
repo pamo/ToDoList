d6165227eb944e84f0a9aa660f1c4b9dc207c8e5
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-10-09 20:33:18 -0700

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Pamela Ocampo on 10/9/13.
+//  Copyright (c) 2013 Pamela Ocampo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToDoApp
+//
+//  Created by Pamela Ocampo on 10/9/13.
+//  Copyright (c) 2013 Pamela Ocampo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
